69c67eeefa1072f6349dccd89fa19ca7f5d5a8ca
sahil <1rn21cs130.sahil@gmail.com>
2026-07-07 00:00:04 +0530

    Updated the SQLAlchemy model to sync with DB

--- app/models/db_models.py
+++ app/models/db_models.py
@@ -2,6 +2,9 @@ from sqlalchemy import Column, Integer, String, Text
 
 from app.core.database import Base
 
+from sqlalchemy import Column, DateTime
+from sqlalchemy.sql import func
+
 
 class ChatMessage(Base):
 
@@ -9,10 +12,19 @@ class ChatMessage(Base):
 
     id = Column(Integer, primary_key=True, index=True)
 
-    conversation_id = Column(String, index=True)
+    conversation_id = Column(
+    String,
+    index=True,
+    nullable=False
+)
+    
 
     role = Column(String)
 
     content = Column(Text)
 
-    created_at = Column(String, default="")  # You can use a timestamp or datetime type if needed
+    created_at = Column(
+    DateTime,
+    server_default=func.now()
+)
+    
